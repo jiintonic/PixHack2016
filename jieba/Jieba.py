57a9fa8d940b23f24dbbac61388f4c475153a461
Albert Cheng <musicmilif@gmail.com>
2016-06-28 10:21:50 +0800

    [MODIFY] Jieba and dictionary

--- jieba/Jieba.py
+++ jieba/Jieba.py
@@ -2,6 +2,12 @@ import jieba
 import jieba.analyse
 import glob
 import re
+import codecs
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 path = '/Users/albertcheng/Documents/Projects/Pixnet_hackathon_2016/pixnet/'
 
@@ -15,26 +21,30 @@ def get_stopWords(stopwordFile):
     return stopWords_set
 jieba.set_dictionary('/Users/albertcheng/Documents/Projects/20151123_NLP/dict.txt.big.txt')
 
-# Load files from path, and save to dictionary
-textfile = {}
-for index, doc in enumerate(glob.glob(path+'*.txt')):
-    content = open(doc, 'rb').read()
-    textfile[index] = []
-    for line in content.splitlines():
-        line = re.sub('[0-9.]', '', line)
-        line = re.sub('www[\w./-]+', '', line)
-        line = re.sub('http[:\w./-]+', '', line)
-        textfile[index].append(line.strip().decode('utf-8'))
-    textfile[index] = ''.join(textfile[index])
+# Load files from path, and save to text file
+fileName = path+'test/test.txt'
 
-# jieba cut!!! and filter stopwords
-for index in xrange(len(textfile)):
-    seg_words = jieba.cut(textfile[index], cut_all=False, HMM=True)
-    words=[]
-    stop_sc = get_stopWords(stopwordFile)
-    for word in seg_words:
-        if word.strip() not in stop_sc:
-            words.append(word.strip())
-        else:
-            pass
-    print ' '.join(words)
+with open(fileName, 'w+') as f:
+    # Read article one by one, memory friendly
+    for doc in glob.glob(path+'*.txt'):
+        # tmp: save content without new line, http
+        tmp = []
+        content = open(doc, 'rb').read()
+        for line in content.splitlines():
+            line = re.sub('[0-9.]', '', line)
+            line = re.sub('www[\w./-_]+', '', line)
+            line = re.sub('http[:\w./-_]+', '', line)
+            tmp.append(line.strip())
+        tmp = ''.join(tmp)
+        seg_words = jieba.cut(tmp, cut_all=False, HMM=True)
+        # words: save content after jieba and remove stop words, words separate by blank
+        words = []
+        stop_sc = get_stopWords(stopwordFile)
+        for word in seg_words:
+            if word.strip() not in stop_sc:
+                words.append(word.strip().encode('utf-8'))
+            else:
+                pass
+        words[0]
+        f.write(re.sub('\s+', ' ', ' '.join(words))+'\n')
+    f.close()
